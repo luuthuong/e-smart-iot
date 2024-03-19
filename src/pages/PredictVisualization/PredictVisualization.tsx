65d4a92f9299ea7efed24954b28ef31a89779532
ThuongNguyen <luuthuong20022000@gmail.com>
2024-03-19 21:54:54 +0700

    update predict chart

--- src/pages/PredictVisualization/PredictVisualization.tsx
+++ src/pages/PredictVisualization/PredictVisualization.tsx
@@ -9,7 +9,7 @@ import {
     IonRow,
     IonTitle,
 } from "@ionic/react";
-import { homeOutline, refreshOutline } from "ionicons/icons";
+import { homeOutline, refreshOutline, stopCircleOutline } from "ionicons/icons";
 import * as tf from "@tensorflow/tfjs";
 import Chart, { Props } from "react-apexcharts";
 import {
@@ -108,32 +108,17 @@ export const PredictVisualization = () => {
         });
     };
 
-    function createData(
-        name: string,
-        calories: number,
-        fat: number,
-        carbs: number
-    ) {
-        return { name, calories, fat, carbs };
-    }
-
-    const rows = [
-        createData("Temperature", 159, 6.0, 24),
-        createData("Soil", 237, 9.0, 37),
-        createData("Light", 262, 16.0, 24),
-    ];
-
     const [predicted, setPredicted] = useState<PredictHistory | undefined>(
         undefined
     );
     useEffect(() => {
         const _ref = collection(firestore, PREDICT_HISTORY);
-        getDocs(query(_ref, orderBy("time"), limit(1))).then((res) => {
+        getDocs(query(_ref, orderBy("time", "desc"), limit(1))).then((res) => {
             const result = res.docs.map(
                 (item) =>
                     ({
                         ...item.data(),
-                        time: item.data().time.toDate(),
+                        time: item.data().time,
                     } as PredictHistory)
             );
             if (!result?.length) return;
@@ -161,74 +146,128 @@ export const PredictVisualization = () => {
         });
     }, []);
 
-    useEffect(() => {
+    const onPredict = () => {
         if (modelType === ModelPredict.NONE) return;
         const keys: (keyof Sensor)[] = ["light", "soil", "temperature"];
         const loading = new Set(keys);
         if (chartLoading.length != 0) return;
         setChartLoading(keys);
-        getData().then(async (result) => {
-            Promise.all(
-                keys.map((key) =>
-                    PredictFactory[modelType](result.map((x) => x[key]))
-                )
-            )
-                .then((responses) => {
-                    responses.forEach((res, index) => {
-                        const predict = res.map(Math.round);
-                        setSerieState((curr) => ({
-                            ...curr,
-                            [keys[index]]: [
-                                {
-                                    data: [...predict] as number[],
-                                },
-                            ] as ApexAxisChartSeries,
-                        }));
-                        loading.delete(keys[index]);
-                        setChartLoading([...loading]);
-                    });
 
-                    const _keys: (keyof Omit<
-                        PredictHistory,
-                        "time" | "timeRange"
-                    >)[] = ["light", "soil", "temperature"];
-                    return responses.reduce(
-                        (prev: PredictHistory, cur, index) => {
-                            prev[_keys[index]] = [...cur] as number[];
-                            return prev;
-                        },
-                        {} as PredictHistory
-                    );
-                })
-                .then((data) => {
-                    data.time = Timestamp.fromDate(new Date());
-                    data.timeRange = getNext7Days();
-                    return Promise.all([
-                        data,
-                        addDoc(collection(firestore, PREDICT_HISTORY), data),
-                    ]);
-                })
-                .then((res) => {
-                    setPredicted(res[0]);
-                    console.log("saved predict", res[1].id);
-                })
-                .catch((e) => {
-                    console.log("error when saved data", e);
-                });
-        });
+        getData()
+            .then(async (result) => {
+                Promise.all(
+                    keys.map((key) =>
+                        PredictFactory[modelType](result.map((x) => x[key]))
+                    )
+                )
+                    .then((responses) => {
+                        responses.forEach((res, index) => {
+                            if (!res.length) return;
+                            const predict = res.map(Math.round);
+                            setSerieState((curr) => ({
+                                ...curr,
+                                [keys[index]]: [
+                                    {
+                                        data: [...predict] as number[],
+                                    },
+                                ] as ApexAxisChartSeries,
+                            }));
+                            loading.delete(keys[index]);
+                            setChartLoading([...loading]);
+                        });
+
+                        const _keys: (keyof Omit<
+                            PredictHistory,
+                            "time" | "timeRange"
+                        >)[] = ["light", "soil", "temperature"];
+                        return responses.reduce(
+                            (prev: PredictHistory, cur, index) => {
+                                prev[_keys[index]] = [...cur] as number[];
+                                return prev;
+                            },
+                            {} as PredictHistory
+                        );
+                    })
+                    .then((data) => {
+                        data.time = Timestamp.fromDate(new Date());
+                        data.timeRange = getNext7Days();
+                        return Promise.all([
+                            data,
+                            addDoc(
+                                collection(firestore, PREDICT_HISTORY),
+                                data
+                            ),
+                        ]);
+                    })
+                    .then((res) => {
+                        setPredicted(res[0]);
+                        console.log("saved predict", res[1].id);
+                    })
+                    .catch((e) => {
+                        console.log("error when saved data", e);
+                    });
+            })
+            .finally(() => {});
+    };
+
+    useEffect(() => {
+        onPredict();
     }, [modelType]);
+
+    const isActiveModel = (type: ModelPredict) => modelType === type;
 
     return (
         <div className={"px-4 w-full"}>
             <div className={"mt-2 flex  justify-end mr-2 items-center"}>
                 <IonTitle className={"font-bold text-gray-600 text-2xl"}>
-                    Predict visualization for next week
+                    Predict visualization
                 </IonTitle>
                 <IonButtons>
-                    <IonButton color={"light"} fill={"solid"}>
-                        <IonIcon slot={"start"} icon={refreshOutline} />
-                        <span>Refresh</span>
+                    <IonButton
+                        disabled={chartLoading.length != 0}
+                        onClick={() => setModelType(() => ModelPredict.LINEAR)}
+                        color={
+                            isActiveModel(ModelPredict.LINEAR)
+                                ? "dark"
+                                : "light"
+                        }
+                        fill={
+                            !isActiveModel(ModelPredict.LINEAR)
+                                ? "solid"
+                                : "outline"
+                        }
+                    >
+                        <span className="text-gray-700">Linear</span>
                     </IonButton>
+                    <IonButton
+                        disabled={chartLoading.length != 0}
+                        onClick={() => setModelType(() => ModelPredict.RNN)}
+                        color={
+                            isActiveModel(ModelPredict.RNN) ? "dark" : "light"
+                        }
+                        fill={
+                            !isActiveModel(ModelPredict.RNN)
+                                ? "solid"
+                                : "outline"
+                        }
+                    >
+                        <span className="text-gray-700">RNN</span>
+                    </IonButton>
+                    {
+                    (modelType) ? (
+                        <IonButton
+                            disabled={!!chartLoading.length}
+                            onClick={onPredict}
+                            color={"light"}
+                            fill={"solid"}
+                        >
+                            <IonIcon slot={"start"} icon={refreshOutline} />
+                            <span>Refresh</span>
+                        </IonButton>
+                    )
+                    : <></>
+                }
+
                     <IonRouterLink routerLink={"/system"}>
                         <IonButton color={"light"} fill={"solid"}>
                             <IonIcon slot={"start"} icon={homeOutline} />
@@ -237,20 +276,6 @@ export const PredictVisualization = () => {
                     </IonRouterLink>
                 </IonButtons>
             </div>
-            <FormControl disabled={chartLoading.length != 0} className="my-2">
-                <InputLabel id="demo-simple-select-label">Model</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    value={modelType}
-                    onChange={(e) =>
-                        setModelType(e.target.value as ModelPredict)
-                    }
-                >
-                    <MenuItem value={ModelPredict.NONE}>NONE</MenuItem>
-                    <MenuItem value={ModelPredict.RNN}>RNN</MenuItem>
-                    <MenuItem value={ModelPredict.LINEAR}>Linear</MenuItem>
-                </Select>
-            </FormControl>
             <div
                 className={
                     "mt-4 flex gap-2 justify-center w-full mx-auto flex-wrap"
@@ -276,59 +301,75 @@ export const PredictVisualization = () => {
                     </div>
                 ))}
             </div>
-            <h1 className="text-gray-600 text-2xl font-bold">Last predict</h1>
+
             {predicted && (
-                <TableContainer className="mb-10" component={Paper}>
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Sensors</TableCell>
-                                {(predicted?.timeRange || getNext7Days()).map(
-                                    (item, index) => (
+                <>
+                    <div className="flex gap-4 items-center">
+                        <h1 className="text-gray-600 text-2xl font-bold">
+                            Last predict
+                        </h1>
+                        <span>
+                            {moment(predicted.time.toDate()).format(
+                                "DD-MM-yyyy HH:mm:ss"
+                            )}
+                        </span>
+                    </div>
+                    <TableContainer className="mb-10" component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Sensors</TableCell>
+                                    {(
+                                        predicted?.timeRange || getNext7Days()
+                                    ).map((item, index) => (
                                         <TableCell key={index} align="right">
                                             {item}
                                         </TableCell>
+                                    ))}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {Object.keys(predicted)
+                                    .filter(
+                                        (x) =>
+                                            !["time", "timeRange"].includes(x)
                                     )
-                                )}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {Object.keys(predicted)
-                                .filter(
-                                    (x) => !["time", "timeRange"].includes(x)
-                                )
-                                .map((key, index) => (
-                                    <TableRow
-                                        key={index}
-                                        sx={{
-                                            "&:last-child td, &:last-child th":
-                                                {
-                                                    border: 0,
-                                                },
-                                        }}
-                                    >
-                                        <TableCell component="th" scope="row">
-                                            {key.toUpperCase()}
-                                        </TableCell>
-                                        {(predicted[key] as number[]).map(
-                                            (item, i) => (
-                                                <TableCell
-                                                    key={i}
-                                                    component="th"
-                                                    scope="row"
-                                                    style={{
-                                                        textAlign: "center",
-                                                    }}
-                                                >
-                                                    {Math.round(item)}
-                                                </TableCell>
-                                            )
-                                        )}
-                                    </TableRow>
-                                ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                                    .map((key, index) => (
+                                        <TableRow
+                                            key={index}
+                                            sx={{
+                                                "&:last-child td, &:last-child th":
+                                                    {
+                                                        border: 0,
+                                                    },
+                                            }}
+                                        >
+                                            <TableCell
+                                                component="th"
+                                                scope="row"
+                                            >
+                                                {key.toUpperCase()}
+                                            </TableCell>
+                                            {(predicted[key] as number[]).map(
+                                                (item, i) => (
+                                                    <TableCell
+                                                        key={i}
+                                                        component="th"
+                                                        scope="row"
+                                                        style={{
+                                                            textAlign: "center",
+                                                        }}
+                                                    >
+                                                        {Math.round(item)}
+                                                    </TableCell>
+                                                )
+                                            )}
+                                        </TableRow>
+                                    ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </>
             )}
         </div>
     );
